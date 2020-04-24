ebe8308cc7c8869f4b0ba5bc5d02c5aff40242f0
Unknown <halit.talha.ture@gmail.com>
2020-04-24 23:54:44 +0300

    Initial commit

---
+++ src/main/java/com/talhature/graylog/fatjar/Main.java
@@ -0,0 +1,14 @@
+package com.talhature.graylog.fatjar;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		logger.info("HELLO GRAYLOG");
+	}
+
+}
